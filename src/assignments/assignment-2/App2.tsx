11573af27eb93a586187081ce07b79fa6410e932
Deep Kothari <deepk2891@gmail.com>
2024-01-20 16:40:07 +0530

    Update assignment-2 counter

--- src/assignments/assignment-2/App2.tsx
+++ src/assignments/assignment-2/App2.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { FcAlarmClock } from "react-icons/fc"
 
 const App2 = () => {
 	const [time, setTime] = useState(0)
@@ -36,16 +37,28 @@ const App2 = () => {
 		const minutes = Math.floor((timeInSeconds % 3600) / 60)
 		const seconds = timeInSeconds % 60
 
-		return `${hours} ${minutes} ${seconds}`
+		return `${hours} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`
 	}
 
 	return (
-		<>
-			<h1 className="font-3xl">{formatTime(time)}</h1>
-			<button onClick={handleStart}>Start</button>
-			<button onClick={handleStop}>Stop</button>
-			<button onClick={handleReset}>Reset</button>
-		</>
+		<div className="container mx-auto">
+			<div>
+				<h1 className="text-2xl font-bold bg-gradient-to-tr from-indigo-600 to-green-600 bg-clip-text text-transparent hover:cursor-pointer pt-7 ps-10">Assignment-2</h1>
+			</div>
+			<div className="text-center mt-8">
+				<FcAlarmClock className="inline text-5xl my-2" />
+				<h1 className="text-3xl font-bold mb-4">{formatTime(time)}</h1>
+				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleStart}>
+					Start
+				</button>
+				<button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleStop}>
+					Stop
+				</button>
+				<button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={handleReset}>
+					Reset
+				</button>
+			</div>
+		</div>
 	)
 }
 
