cacc2d1e09ae0dc90af6bdeebaae23df1a628ea5
Deep Kothari <deepk2891@gmail.com>
2024-01-20 16:20:18 +0530

    Added assignment-2 counter

--- src/assignments/assignment-2/App2.tsx
+++ src/assignments/assignment-2/App2.tsx
@@ -1,7 +1,52 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 const App2 = () => {
-	return <div>App2</div>
+	const [time, setTime] = useState(0)
+	const [isRunning, setIsRunning] = useState(false)
+
+	useEffect(() => {
+		let interval
+
+		if (isRunning) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime + 1)
+			}, 1000)
+		} else {
+			clearInterval(interval)
+		}
+
+		return () => {
+			clearInterval(interval)
+		}
+	}, [isRunning])
+
+	const handleStart = () => {
+		setIsRunning(true)
+	}
+	const handleStop = () => {
+		setIsRunning(false)
+	}
+	const handleReset = () => {
+		setIsRunning(false)
+		setTime(0)
+	}
+
+	const formatTime = (timeInSeconds: number) => {
+		const hours = Math.floor(timeInSeconds / 3600)
+		const minutes = Math.floor((timeInSeconds % 3600) / 60)
+		const seconds = timeInSeconds % 60
+
+		return `${hours} ${minutes} ${seconds}`
+	}
+
+	return (
+		<>
+			<h1 className="font-3xl">{formatTime(time)}</h1>
+			<button onClick={handleStart}>Start</button>
+			<button onClick={handleStop}>Stop</button>
+			<button onClick={handleReset}>Reset</button>
+		</>
+	)
 }
 
 export default App2
