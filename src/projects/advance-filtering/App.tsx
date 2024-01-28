1bef3a6c9988211456f447994364f42fcc80fbc7
Deep Kothari <deepk2891@gmail.com>
2024-01-28 19:03:19 +0530

    Created Navbar

--- src/projects/advance-filtering/App.tsx
+++ src/projects/advance-filtering/App.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import "./App.css"
+import Nav from "./Navigation/Nav"
+import Sidebar from "./Sidebar/Sidebar"
+import Products from "./Products/Products"
+import Recommended from "./Recommended/Recommended"
 
 const Filtering = () => {
-	return <div className="">Filtering</div>
+	return (
+		<>
+			<Nav />
+			<Sidebar />
+			<Products />
+			<Recommended />
+		</>
+	)
 }
 
 export default Filtering
--- src/projects/advance-filtering/Navigation/Nav.jsx
+++ src/projects/advance-filtering/Navigation/Nav.jsx
@@ -1,8 +1,37 @@
 import React from "react"
 import "./Nav.css"
+import { AiOutlineShoppingCart, AiOutlineUserAdd, FiHeart, FaBars } from "react-icons/ai"
 
 const Nav = () => {
-	return <div>Nav</div>
+	return (
+		<>
+			<nav className="py-4">
+				<div className="container mx-auto flex justify-between align-middle">
+					<input
+						type="text"
+						placeholder="search Footwear"
+						className="w-40 md:w-72 text-lg h-8 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none text-gray-700 px-4 py-5 leading-8 transition-colors duration-200 ease-in-out"
+					/>
+					<div className="align-middle mt-3 text-xl md:text-2xl hidden md:block">
+						<div className="flex space-x-6">
+							<a href="#">
+								<FiHeart className="nav-icons" />
+							</a>
+							<a href="#">
+								<AiOutlineShoppingCart className="nav-icons" />
+							</a>
+							<a href="#">
+								<AiOutlineUserAdd className="nav-icons" />
+							</a>
+						</div>
+					</div>
+					<div className="mt-3 text-xl block md:hidden">
+						<FaBars />
+					</div>
+				</div>
+			</nav>
+		</>
+	)
 }
 
 export default Nav
