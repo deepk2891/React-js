01ab6dd9ab6ae207c9013d82478a224929a0b586
Deep Kothari <deepk2891@gmail.com>
2024-01-28 17:35:25 +0530

    Advance Filtering File setup

--- src/projects/advance-filtering/App.tsx
+++ src/projects/advance-filtering/App.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import "./App.css"
 
 const Filtering = () => {
-	return <div>Filtering</div>
+	return <div className="">Filtering</div>
 }
 
 export default Filtering
---
+++ src/projects/advance-filtering/Products/Products.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Products.css"
+
+const products = () => {
+	return <div>products</div>
+}
+
+export default products
