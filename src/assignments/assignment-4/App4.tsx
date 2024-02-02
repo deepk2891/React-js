9d45d2d5eef4c559ae1f3ae4fcd30ce5626c7b2f
Deep Kothari <deepk2891@gmail.com>
2024-02-02 16:01:41 +0530

    Update Feedback form

--- src/assignments/assignment-4/App4.tsx
+++ src/assignments/assignment-4/App4.tsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react"
-import Starrating from "./Starrating"
 import { FaStar } from "react-icons/fa"
+import "./style.css"
 
 interface Testimonial {
 	name: string
 	rating: number
 	message: string
 }
-
-const App4: React.FC = () => {
+const App4 = () => {
 	const [formData, setFormData] = useState({
 		name: "",
 		rating: 4,
 		message: "",
 	})
 
-	const [testimonials, setTestimonials] = useState<Array<Testimonial>>([])
+	const [testimonials, setTestimonials] = useState([])
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+	const handleInputChange = (e) => {
 		const { name, value } = e.target
 		setFormData({ ...formData, [name]: value })
 	}
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	const handleSubmit = (e) => {
 		e.preventDefault()
 
-		const newTestimonial: Testimonial = {
+		const newTestimonial = {
 			name: formData.name,
 			rating: formData.rating,
 			message: formData.message,
@@ -39,70 +38,69 @@ const App4: React.FC = () => {
 		setTestimonials([...testimonials, newTestimonial])
 
 		setFormData({ name: "", rating: 0, message: "" })
-		console.log(formData.rating)
 	}
 
-	const generateStars = (rating: number): React.ReactNode[] => {
+	const generateStars = (rating) => {
 		return Array.from({ length: rating }, (_, index) => <FaStar key={index} className="inline" />)
 	}
 
 	return (
-		<>
-			<div className="container mx-auto">
-				<h1 className="text-2xl py-5 text-center font-semibold text-indigo-600">Send us Feedback</h1>
-				<div className="flex flex-wrap">
-					<form className="w-full lg:w-1/2 p-5" onSubmit={handleSubmit}>
-						<h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-						<div className="relative mb-4">
-							<label className="leading-7 text-sm text-gray-600">Name</label>
-							<input
-								type="text"
-								id="name"
-								name="name"
-								className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-								onChange={handleInputChange}
-								value={formData.name}
-							/>
-						</div>
-						<div className="relative mb-4">
-							<label className="leading-7 text-sm text-gray-600">Rating</label>
-							<Starrating value={formData.rating} onChange={(value) => setFormData({ ...formData, rating: value })} />
-						</div>
-						<div className="relative mb-4">
-							<label className="leading-7 text-sm text-gray-600">Message</label>
-							<textarea
-								id="message"
-								name="message"
-								className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-								onChange={handleInputChange}
-								value={formData.message}
-							/>
-						</div>
-						<button type="submit" className="w-full text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-							Submit
-						</button>
-						<p className="text-xs text-gray-500 mt-3">Thank you for your response, we will contact you as soon as possible if you have any queries.</p>
-					</form>
-					<div className="w-full lg:w-1/2 p-5">
-						{testimonials.map((testimonial, index) => (
-							<div key={index} className="bg-gray-100 p-8 my-4 rounded">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
-									<path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-								</svg>
-								<p className="leading-relaxed mb-6">{testimonial.message}</p>
-								<div className="inline-flex align-middle">
-									<img src="https://dummyimage.com/60x60/ce4c4c/000" className="rounded-full" alt="" />
-									<p className="flex flex-col justify-center gap-1 align-middle pl-4">
-										<span className="title-font font-medium text-gray-900">{testimonial.name}</span>
-										<span className="text-sm flex"> {generateStars(testimonial.rating)}</span>
-									</p>
-								</div>
-							</div>
+		<div className="container mx-auto">
+			<h1 className="text-2xl py-5 text-center font-semibold text-indigo-600">Send us Feedback</h1>
+			<div className="flex flex-wrap">
+				<form className="w-full lg:w-1/2 p-5" onSubmit={handleSubmit}>
+					<h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+					<div className="mb-4">
+						<label className="leading-7 text-sm text-gray-600">Name</label>
+						<input
+							type="text"
+							name="name"
+							className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							onChange={handleInputChange}
+							value={formData.name}
+						/>
+					</div>
+					<div className="mb-4">
+						<label className="leading-7 text-sm text-gray-600">Rating</label>
+						{[1, 2, 3, 4, 5].map((index) => (
+							<label key={index} className={`star ${index <= formData.rating ? "on" : "off"}`}>
+								<input type="radio" name="rating" value={index} checked={index === formData.rating} onChange={() => setFormData({ ...formData, rating: index })} />
+								<span className="star text-2xl">
+									<FaStar className="inline" />
+								</span>
+							</label>
 						))}
 					</div>
+					<div className="mb-4">
+						<label className="leading-7 text-sm text-gray-600">Message</label>
+						<textarea
+							name="message"
+							className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+							onChange={handleInputChange}
+							value={formData.message}
+						/>
+					</div>
+					<button type="submit" className="w-full text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+						Submit
+					</button>
+					<p className="text-xs text-gray-500 mt-3">Thank you for your response, we will contact you as soon as possible if you have any queries.</p>
+				</form>
+				<div className="w-full lg:w-1/2 p-5">
+					{testimonials.map((testimonial, index) => (
+						<div key={index} className="bg-gray-100 p-8 my-4 rounded">
+							<p className="leading-relaxed mb-6">{testimonial.message}</p>
+							<div className="inline-flex align-middle">
+								<img src="https://dummyimage.com/60x60/ce4c4c/000" className="rounded-full" alt="" />
+								<p className="flex flex-col justify-center gap-1 align-middle pl-4">
+									<span className="title-font font-medium text-gray-900">{testimonial.name}</span>
+									<span className="text-sm flex"> {generateStars(testimonial.rating)}</span>
+								</p>
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
